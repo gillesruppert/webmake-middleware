0c979bd038121b6346e9c90991660f2f3245745f
Gilles Ruppert <gilles@madeofbytes.com>
2012-08-24 22:46:14 +0200

    change port number to make travis ci pass

--- test/connect-test.js
+++ test/connect-test.js
@@ -11,17 +11,17 @@ describe('connect middleware', function() {
 
   it('should return the compiled file when requesting a valid url ', function(done) {
     var path = normalize(__dirname + '/fixtures/main.js');
-    var server = connect().use(webmakeMw('/test.js', path)).listen(3000, function(err) {
+    var server = connect().use(webmakeMw('/test.js', path)).listen(51234, function(err) {
       if (err) throw err;
-      http.get('http://localhost:3000/test.js', helpers.successResponseCallback(done, server.close.bind(server)));
+      http.get('http://localhost:51234/test.js', helpers.successResponseCallback(done, server.close.bind(server)));
     });
   });
 
   it('should return a 500 response when webmake has a problem with compiling the script', function(done) {
     var path = normalize(__dirname + '/fixtures/err.js');
-    var server = connect().use(webmakeMw('/error.js', path)).listen(3000, function(err) {
+    var server = connect().use(webmakeMw('/error.js', path)).listen(51234, function(err) {
       if (err) throw err;
-      http.get('http://localhost:3000/error.js', helpers.failureResponseCallback(done, server.close.bind(server)));
+      http.get('http://localhost:51234/error.js', helpers.failureResponseCallback(done, server.close.bind(server)));
     });
   });
 
@@ -30,11 +30,11 @@ describe('connect middleware', function() {
       '/test1.js': normalize(__dirname + '/fixtures/main.js'),
       '/test2.js': normalize(__dirname + '/fixtures/page2.js')
     };
-    var server = connect().use(webmakeMw(config)).listen(3000);
+    var server = connect().use(webmakeMw(config)).listen(51234);
 
     async.parallel([
-      function(cb) { http.get('http://localhost:3000/test1.js', helpers.successResponseCallback(cb)); },
-      function(cb) { http.get('http://localhost:3000/test2.js', helpers.successResponseCallback(cb)); }
+      function(cb) { http.get('http://localhost:51234/test1.js', helpers.successResponseCallback(cb)); },
+      function(cb) { http.get('http://localhost:51234/test2.js', helpers.successResponseCallback(cb)); }
     ], function(err) {
       if (err) throw err;
       server.close(done);
