83ead2f2bc433597a06e9a015a7a045edaf47d30
Mariusz Nowak <mariusz@medikoo.com>
2014-03-24 23:18:33 +0100

    -- lint

--- test/configuration-test.js
+++ test/configuration-test.js
@@ -1,24 +1,24 @@
 'use strict';
 
-var expect = require('expect.js');
-var webmakeMw = require('../');
+var expect = require('expect.js'), webmakeMw = require('../');
 
-describe('webmake middleware', function() {
-  describe('configuration function', function() {
-    it('should be a function which takes to arguments', function() {
+describe('webmake middleware', function () {
+  describe('configuration function', function () {
+    it('should be a function which takes to arguments', function () {
       expect(webmakeMw).to.be.a('function');
       expect(webmakeMw).to.have.length(2);
     });
 
-    it('should return a middelware function which takes 3 arguments', function() {
-      expect(webmakeMw({})).to.be.a('function');
-      expect(webmakeMw({})).to.have.length(3);
-    });
+    it('should return a middelware function which takes 3 arguments',
+      function () {
+        expect(webmakeMw({})).to.be.a('function');
+        expect(webmakeMw({})).to.have.length(3);
+      });
 
-    it('should take 2 strings as arguments', function() {
-      function err1() { webmakeMw() }
-      function err2() { webmakeMw(1,2,3) }
-      function err3() { webmakeMw(1,2) }
+    it('should take 2 strings as arguments', function () {
+      function err1() { webmakeMw(); }
+      function err2() { webmakeMw(1, 2, 3); }
+      function err3() { webmakeMw(1, 2); }
 
       expect(webmakeMw('foo', 'bar')).to.be.a('function');
       expect(err1).to.throwError();
@@ -26,7 +26,7 @@ describe('webmake middleware', function() {
       expect(err3).to.throwError();
     });
 
-    it('should take 1 object as argument', function() {
+    it('should take 1 object as argument', function () {
       expect(webmakeMw({})).to.be.a('function');
     });
   });
--- test/connect-test.js
+++ test/connect-test.js
@@ -1,44 +1,55 @@
-var expect = require('expect.js');
-var connect = require('connect');
-var http = require('http');
-var normalize = require('path').normalize;
-var async = require('async');
+'use strict';
 
-var webmakeMw = require('../');
-var helpers = require('./helpers');
+var connect = require('connect')
+  , http = require('http')
+  , normalize = require('path').normalize
+  , async = require('async')
 
-describe('connect middleware', function() {
+  , webmakeMw = require('../')
+  , helpers = require('./helpers');
 
-  it('should return the compiled file when requesting a valid url ', function(done) {
-    var path = normalize(__dirname + '/fixtures/main.js');
-    var server = connect().use(webmakeMw('/test.js', path)).listen(51234, function(err) {
-      if (err) throw err;
-      http.get('http://localhost:51234/test.js', helpers.successResponseCallback(done, server.close.bind(server)));
+describe('connect middleware', function () {
+
+  it('should return the compiled file when requesting a valid url ',
+    function (done) {
+      var path = normalize(__dirname + '/fixtures/main.js')
+        , server = connect().use(webmakeMw('/test.js', path)).listen(51234,
+        function (err) {
+          if (err) throw err;
+          http.get('http://localhost:51234/test.js',
+            helpers.successResponseCallback(done, server.close.bind(server)));
+        });
     });
-  });
 
-  it('should return a 500 response when webmake has a problem with compiling the script', function(done) {
-    var path = normalize(__dirname + '/fixtures/err.js');
-    var server = connect().use(webmakeMw('/error.js', path)).listen(51234, function(err) {
-      if (err) throw err;
-      http.get('http://localhost:51234/error.js', helpers.failureResponseCallback(done, server.close.bind(server)));
+  it('should return a 500 response when webmake has a problem with compiling' +
+    ' the script', function (done) {
+      var path = normalize(__dirname + '/fixtures/err.js')
+        , server = connect().use(webmakeMw('/error.js', path)).listen(51234,
+        function (err) {
+          if (err) throw err;
+          http.get('http://localhost:51234/error.js',
+            helpers.failureResponseCallback(done, server.close.bind(server)));
+        });
     });
-  });
 
-  it('should be configurable with an object', function(done){
+  it('should be configurable with an object', function (done) {
     var config = {
       '/test1.js': normalize(__dirname + '/fixtures/main.js'),
       '/test2.js': normalize(__dirname + '/fixtures/page2.js')
-    };
-    var server = connect().use(webmakeMw(config)).listen(51234);
+    }
+      , server = connect().use(webmakeMw(config)).listen(51234);
 
     async.parallel([
-      function(cb) { http.get('http://localhost:51234/test1.js', helpers.successResponseCallback(cb)); },
-      function(cb) { http.get('http://localhost:51234/test2.js', helpers.successResponseCallback(cb)); }
-    ], function(err) {
+      function (cb) {
+        http.get('http://localhost:51234/test1.js',
+          helpers.successResponseCallback(cb));
+      },
+      function (cb) {
+        http.get('http://localhost:51234/test2.js',
+          helpers.successResponseCallback(cb));
+      }], function (err) {
       if (err) throw err;
       server.close(done);
     });
   });
 });
-
