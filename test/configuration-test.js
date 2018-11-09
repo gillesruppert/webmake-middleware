54cb5bd684c4c7c18ed2d06aa74528ba3aa79532
Mariusz Nowak <medyk@medikoo.com>
2018-11-09 15:38:56 +0100

    style: prettier

--- test/configuration-test.js
+++ test/configuration-test.js
@@ -4,22 +4,21 @@ const expect = require("expect.js");
 const webmakeMw = require("../");
 
 describe("webmake middleware", () => {
-  describe("configuration function", () => {
-    it("should be a function which takes to arguments", () => {
-      expect(webmakeMw).to.be.a("function");
-      expect(webmakeMw).to.have.length(1);
-    });
+	describe("configuration function", () => {
+		it("should be a function which takes to arguments", () => {
+			expect(webmakeMw).to.be.a("function");
+			expect(webmakeMw).to.have.length(1);
+		});
 
-    it("should return a middelware function which takes 3 arguments",
-      () => {
-        expect(webmakeMw({})).to.be.a("function");
-        expect(webmakeMw({})).to.have.length(3);
-      });
+		it("should return a middelware function which takes 3 arguments", () => {
+			expect(webmakeMw({})).to.be.a("function");
+			expect(webmakeMw({})).to.have.length(3);
+		});
 
-    it("should take 1 object as argument", () => {
-      function err1() { webmakeMw(); }
-      expect(err1).to.throwError();
-      expect(webmakeMw({})).to.be.a("function");
-    });
-  });
+		it("should take 1 object as argument", () => {
+			function err1() { webmakeMw(); }
+			expect(err1).to.throwError();
+			expect(webmakeMw({})).to.be.a("function");
+		});
+	});
 });
